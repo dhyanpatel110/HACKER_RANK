2e112f4251166d11ec78f64c02896c774863473c
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-11 13:21:44 +0530

    Create Classes.js

---
+++ 10 Days of Javascript/Day 4/Classes.js
@@ -0,0 +1,13 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+function Polygon(shape) {
+  this.type = shape;
+  this.perimeter = getPerimeter;
+}
+
+function getPerimeter() {
+  return this.type.reduce((a, b) => a + b);
+}

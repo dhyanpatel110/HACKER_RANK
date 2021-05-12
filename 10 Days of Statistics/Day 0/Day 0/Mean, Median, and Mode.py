815bbae0befe38d5b294b15d9d3292cd30c6c519
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-12 10:58:34 +0530

    Update Mean, Median, and Mode.py

--- 10 Days of Statistics/Day 0/Day 0/Mean, Median, and Mode.py
+++ 10 Days of Statistics/Day 0/Day 0/Mean, Median, and Mode.py
@@ -1,1 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy
+from scipy import stats
 
+size = int(input())
+numbers = list(map(int, input().split()))
+print(numpy.mean(numbers))
+print(numpy.median(numbers))
+print(int(stats.mode(numbers)[0]))

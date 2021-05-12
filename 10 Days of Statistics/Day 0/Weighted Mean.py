4b016a5860d4f39d9440c8bd2ab2d12487244bb5
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-12 11:28:30 +0530

    Update Weighted Mean.py

--- 10 Days of Statistics/Day 0/Weighted Mean.py
+++ 10 Days of Statistics/Day 0/Weighted Mean.py
@@ -1,1 +1,17 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+size = int(input())
+numbers = list(map(int, input().split()))
+weighted = list(map(int, input().split()))
+
+sum_items = 0
+for i in range(size):
+    sum_items = sum_items + (numbers[i] * weighted[i])
+
+print(round(sum_items/sum(weighted), 1))

8c06abcb8ea2ae611d25d55dc84526cfba41d798
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-18 12:11:11 +0530

    Update Geometric Distribution I.py

--- 10 Days of Statistics/Day 4/Geometric Distribution I.py
+++ 10 Days of Statistics/Day 4/Geometric Distribution I.py
@@ -1,1 +1,15 @@
+# Task:
+# The probability that a machine produces a defective product is 1/3.
+# What is the probability that the 1st defect occurs the 5th item inspection?
 
+# The probability of failure of 1 trial q , where q = 1 - p
+
+# Input data
+probability = list(map(int, input().split()))
+p = probability[0] / probability[1]
+q = 1 - p
+n = int(input())
+
+# Get Geometric Distribution
+result = q ** (n - 1) * p
+print(round(result, 3))

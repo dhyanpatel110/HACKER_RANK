af9fbc1b37b302b867b147f5dcb412f34288f55c
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-18 12:14:57 +0530

    Update Geometric Distribution I.py

--- 10 Days of Statistics/Day 4/Geometric Distribution I.py
+++ 10 Days of Statistics/Day 4/Geometric Distribution I.py
@@ -2,6 +2,7 @@
 # The probability that a machine produces a defective product is 1/3.
 # What is the probability that the 1st defect occurs the 5th item inspection?
 
+# Geometric Distribution = q ^ (n-1) * p
 # The probability of failure of 1 trial q , where q = 1 - p
 
 # Input data
@@ -10,6 +11,8 @@ p = probability[0] / probability[1]
 q = 1 - p
 n = int(input())
 
-# Get Geometric Distribution
+# We can show result on the screen
+# The round() function returns a floating point number that is a rounded version of the specified number, with the specified number of decimals.
+# 3 denotes decimal places (i.e., 1.234  format):
 result = q ** (n - 1) * p
 print(round(result, 3))

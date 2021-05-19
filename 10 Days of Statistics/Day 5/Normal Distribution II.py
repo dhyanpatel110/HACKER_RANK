8acc17edcc006748cde9e1939259d12b69b15e4a
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-19 12:47:42 +0530

    Update Normal Distribution II.py

--- 10 Days of Statistics/Day 5/Normal Distribution II.py
+++ 10 Days of Statistics/Day 5/Normal Distribution II.py
@@ -1,1 +1,34 @@
+# Task:
+# The final grades for a Physics exam taken by a large group of students have a mean of mi = 70 and a standard deviation of sigma = 10.
+# If we can approximate the distribution of these grades by a normal distribution, what percentage of the students:
+# 1. Scored higher than 80 (i.e., have a grade > 80)?
+# 2. Passed the test (i.e., have a grade >= 60)?
+# 3. Failed the test (i.e., have a grade < 60)?
+
+# Import library
+import math
+
+# Define functions
+# The math.erf() method returns the error function of a number (find positive and negative number)
+# The math.sqrt() method returns the square root of a number.
+def cumulative(mean, std, value):
+    return 0.5 * (1 + math.erf((value - mean) / (std * math.sqrt(2))))
+
+# Input data
+initial_values = list(map(float, input().split()))
+mean = initial_values[0]
+std = initial_values[1]
+val_first_question = float(input())
+val_sec_third_question = float(input())
+
+
+# We can show result on the screen
+# The round() function returns a floating point number that is a rounded version of the specified number, with the specified number of decimals.
+# 2 denotes decimal places (i.e., 1.23  format):
+print (round(100 - (cumulative(mean, std, val_first_question) * 100), 2))
+print (round(100 - (cumulative(mean, std, val_sec_third_question) * 100), 2))
+print (round(cumulative(mean, std, val_sec_third_question) * 100, 2))
+
+
+
 

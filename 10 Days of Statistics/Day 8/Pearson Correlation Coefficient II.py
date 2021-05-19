45d63091eff5f64cf44e0bec2d5f723469e6ac05
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-19 11:54:53 +0530

    Update Pearson Correlation Coefficient II.py

--- 10 Days of Statistics/Day 8/Pearson Correlation Coefficient II.py
+++ 10 Days of Statistics/Day 8/Pearson Correlation Coefficient II.py
@@ -1,1 +1,32 @@
+# Task:
+# The regression line of y on x is 3x+4y+8=0, and the regression line of x on y is 4x+3y+7=0
+# What is the value of the Pearson correlation coefficient?
+
+regression X:
+3x + 4y + 8 = 0
+     3x + 8 = -4y
+ 3x/4 + 8/4 = -y
+  -3x/4 - 4 =  y (reverse signal)
+         bx = -3/4
+    
+regression Y:
+4x + 3y + 7 = 0
+     3y + 7 = -4x
+ 3y/4 + 7/4 = -x 
+-3y/4 - 7/4 =  x (reverse signal)
+         by = -3/4
+
+r² = bx * by
+r² = -3/4 * -3/4
+r² = 9/16
+r  = +/- 3/4
+if bx = by then
+ r = -3/4
+
+Answer: -3/4
+  
+  
+  
+  
+  
 

78d186795058086067dcfae46288a2a559b644b8
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-06-23 21:48:36 +0530

    Create Loops.py

---
+++ Python/Introduction/Loops.py
@@ -0,0 +1,22 @@
+TASK:
+  The provided code stub reads and integer,n, from STDIN. For all non-negative integers i<n , print i^2.
+
+EXAMPLE:
+  n = 5
+  The list of non-negative integers that are less than n=5 is [0,1,2,3,4]. Print the square of each number on a separate line.
+  0
+  1
+  4
+  9
+  16
+
+CODE:
+  if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
+        
+        
+        
+        
+        

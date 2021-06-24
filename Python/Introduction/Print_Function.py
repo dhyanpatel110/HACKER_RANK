be68546d48f9f4eeb7592cc3fe3e8ae0a11b9b13
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-06-24 22:17:54 +0530

    Create Print_Function.py

---
+++ Python/Introduction/Print_Function.py
@@ -0,0 +1,16 @@
+EXAMPLE:
+  n = 5
+  Print the string 12345.
+
+CODE:
+  if __name__ == '__main__':
+     n = int(input())
+  i=1
+  while i<=n:
+     print(i,end="")
+     i=i + 1
+    
+    
+    
+    
+    

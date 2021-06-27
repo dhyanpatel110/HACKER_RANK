1d18bdedeab6725dc0c7d929ec92624222fbc8d5
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-06-27 14:13:38 +0530

    Update Find_the_Runner_Up_Score.py

--- Python/Basic_Data_Types/Find_the_Runner_Up_Score.py
+++ Python/Basic_Data_Types/Find_the_Runner_Up_Score.py
@@ -1,29 +1,22 @@
+EXPLANATION:
+ Given list is [2,3,6,6,5] . The maximum score is 6, second maximum is 5. Hence, we print 5 as the runner-up score.
 
-C
- __name__ == '__main__':
-
+CODE:
+ if __name__ == '__main__':
     n = int(input())
-
     arr = map(int, input().split())
-
-
-
+    
     # Checks if n is in between 2 and 10.
-
     if( 2 <= n <= 10  ):
-
         # Removes all the duplicates in the list.
-
         tmp = list( set( arr ) )
-
         
-
         # Removes the highest number
-
         tmp.remove( max( tmp ) )
-
         
-
         # Prints the next highest number.
-
         print( max( tmp ) )
+        
+        
+        
+        

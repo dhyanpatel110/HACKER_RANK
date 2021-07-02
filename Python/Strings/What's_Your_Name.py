d1f895b1be04adcfe9f72cd538d3b5aad8f4b891
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-02 21:29:31 +0530

    Create What's_Your_Name.py

---
+++ Python/Strings/What's_Your_Name.py
@@ -0,0 +1,22 @@
+#Sample Input:
+  Ross
+  Taylor
+#Sample Output:
+  Hello Ross Taylor! You just delved into python. 
+#Explanation:
+  The input read by the program is stored as a string data type. A string is a collection of characters.
+  
+#CODE:
+  def print_full_name(a, b):
+    b = b+"!"
+    print("Hello",a, b,"You just delved into python.");
+    
+  if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+    
+    
+    
+    
+    

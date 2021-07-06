903dd104c45e172e52968bed87aff2fdac3328d4
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-06 23:27:33 +0530

    Create Text_Wrap.py

---
+++ Python/Strings/Text_Wrap.py
@@ -0,0 +1,27 @@
+Sample Input:
+  ABCDEFGHIJKLIMNOQRSTUVWXYZ
+  4
+  
+Sample Output:
+  ABCD
+  EFGH
+  IJKL
+  IMNO
+  QRST
+  UVWX
+  YZ
+
+CODE:
+  import textwrap
+  def wrap(string, max_width):
+    return textwrap.fill(string,max_width)
+
+  if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+    
+    
+    
+    
+    

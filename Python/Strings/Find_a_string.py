71848110fe6a84a388e6103d41cbf47b47d6dd16
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-06 23:22:43 +0530

    Create Find_a_string.py

---
+++ Python/Strings/Find_a_string.py
@@ -0,0 +1,28 @@
+Output Format:
+  Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+Sample Input:
+  ABCDCDC
+  CDC
+  
+Sample Output:
+  2
+  
+CODE:
+  def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)-len(sub_string)+1):
+        if (string[i:i+len(sub_string)] == sub_string):
+            count += 1
+    return count
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
+    
+    
+    
+    
+    

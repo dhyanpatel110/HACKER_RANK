61fc8bef645d5e5ee82f115bfa3d708f0479fde3
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-11 23:03:04 +0530

    Create sWAP_cASE.py

---
+++ Python/Strings/sWAP_cASE.py
@@ -0,0 +1,25 @@
+For Example:
+  Www.HackerRank.com → wWW.hACKERrANK.COM
+  Pythonist 2 → pYTHONIST 2 
+
+CODE:
+  def swap_case(sentence):
+    updated_s = ""
+    for c in sentence:
+        if c.isupper():
+            updated_s += c.lower()
+        elif c.islower():
+            updated_s += c.upper()
+        else:
+            updated_s += c
+    return updated_s
+
+ if __name__ == '__main__':
+      s = input()
+      result = swap_case(s)
+      print(result)
+      
+      
+      
+      
+      

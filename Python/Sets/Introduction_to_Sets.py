b3b2358b56f78a1da84a737cd85e2462549e804b
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-20 11:18:14 +0530

    Update Introduction_to_Sets.py

--- Python/Sets/Introduction_to_Sets.py
+++ Python/Sets/Introduction_to_Sets.py
@@ -1,7 +1,8 @@
+'''
 Explanation:
   Here, set[154,161,167,170,171,174,176,182]is the set containing the distinct heights. Using the sum() and len() functions, we can compute the average.
   average = 1355/8 = 169.375
-
+'''
 CODE:
   def average(array):
     # your code goes here

126b77d23bc244de87771ca8db5721c7c7cd4ea0
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-21 11:20:42 +0530

    Update Set_add.py

--- Python/Sets/Set_add.py
+++ Python/Sets/Set_add.py
@@ -1,3 +1,4 @@
+'''
 Task:
   Apply your knowledge of the .add() operation to help your friend Rupal.
   Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of N country stamps.
@@ -14,7 +15,8 @@ Sample Input:
   France 
 Sample Output:
   5
-  
+ '''
+ 
 CODE:
   N = int(input())
   countries = set()

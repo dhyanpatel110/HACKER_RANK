15c07ac84ec811bf06d0aceaa574dbd8727fb408
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-24 22:37:13 +0530

    Update Loops.py

--- Python/Introduction/Loops.py
+++ Python/Introduction/Loops.py
@@ -1,3 +1,4 @@
+'''
 TASK:
   The provided code stub reads and integer,n, from STDIN. For all non-negative integers i<n , print i^2.
 
@@ -9,6 +10,7 @@ EXAMPLE:
   4
   9
   16
+'''
 
 CODE:
   if __name__ == '__main__':

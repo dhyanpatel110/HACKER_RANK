d48a965b180e0ef2928aa8a95d96635cf07ef4a1
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-26 21:58:30 +0530

    Update Write_a_function.py

--- Python/Introduction/Write_a_function.py
+++ Python/Introduction/Write_a_function.py
@@ -1,9 +1,11 @@
+'''
 TASK:
   Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
   Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 
 EXPLANATION:
   1990 is not a multiple of 4 hence it's not a leap year.
+'''
 
 CODE:
   def is_leap(year):

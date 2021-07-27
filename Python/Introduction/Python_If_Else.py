5c2579aba1d11940c4a48d78adaea1772e6ec81c
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-27 10:59:25 +0530

    Update Python_If_Else.py

--- Python/Introduction/Python_If_Else.py
+++ Python/Introduction/Python_If_Else.py
@@ -1,17 +1,16 @@
+'''
 TASK:
-  
-Given an integer,n, perform the following conditional actions:
+  Given an integer,n, perform the following conditional actions:
 
-If n is odd, print Weird
-If n is even and in the inclusive range of 2 to 5, print Not Weird
-If n is even and in the inclusive range of 6 to 20, print Weird
-If n is even and greater than 20, print Not Weird
-
+  If n is odd, print Weird
+  If n is even and in the inclusive range of 2 to 5, print Not Weird
+  If n is even and in the inclusive range of 6 to 20, print Weird
+  If n is even and greater than 20, print Not Weird
 
 EXPLANATION:
-n = 24
-n > 20 and n is even, so it is not weird.
-
+  n = 24
+  n > 20 and n is even, so it is not weird.
+'''
 
 CODE:
 #!/bin/python3
@@ -32,5 +31,6 @@ if __name__ == '__main__':
          ])
     
     
-
-      
+    
+    
+    

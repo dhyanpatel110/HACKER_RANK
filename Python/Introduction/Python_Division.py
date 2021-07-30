6674642d75f91841e8b81e2e7037cfe125c57f62
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-30 22:27:49 +0530

    Update Python_Division.py

--- Python/Introduction/Python_Division.py
+++ Python/Introduction/Python_Division.py
@@ -1,3 +1,4 @@
+'''
 TASK:
   The provided code stub reads two integers, a and b, from STDIN.
   Add logic to print two lines. The first line should contain the result of integer division, a//b . The second line should contain the result of float division, a/b .
@@ -8,7 +9,7 @@ EXPLANATION:
   b = 5
   The result of the integer division 3//5 = 0
   The result of the float division is 3/5 = 0.6
-  
+'''
   
 CODE:
   if __name__ == '__main__':

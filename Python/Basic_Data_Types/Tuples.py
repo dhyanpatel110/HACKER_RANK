89ce8a54901efe05907f090f4949396103de8df8
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-08-06 14:18:58 +0530

    Update Tuples.py

--- Python/Basic_Data_Types/Tuples.py
+++ Python/Basic_Data_Types/Tuples.py
@@ -1,7 +1,9 @@
-#TASK:
+'''
+TASK:
   Given an integer,n, and n space-separated integers as input, create a tuple,t, of those n integers. Then compute and print the result of hash(t).
+'''
 
-#CODE:
+CODE:
   if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())

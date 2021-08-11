dc573470d4ef9502da69516a70262f397c03867d
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-08-11 21:49:32 +0530

    Update What's_Your_Name.py

--- Python/Strings/What's_Your_Name.py
+++ Python/Strings/What's_Your_Name.py
@@ -1,11 +1,13 @@
-#Sample Input:
+'''
+Sample Input:
   Ross
   Taylor
-#Sample Output:
+Sample Output:
   Hello Ross Taylor! You just delved into python. 
-#Explanation:
+Explanation:
   The input read by the program is stored as a string data type. A string is a collection of characters.
-  
+'''
+
 #CODE:
   def print_full_name(a, b):
     b = b+"!"
